e33f3620adddf0c0f041ee874b93ff59ac5feaae
ste <mrbarrow@hotmail.co.uk>
2025-12-07 22:29:33 +0000

    2015:7:2

--- 2015/7/bitwise.py
+++ 2015/7/bitwise.py
@@ -26,4 +26,28 @@ while len(resolveStack) != 0:
 
 print(instructions[charToSolve])
 
-    
+answer = instructions[charToSolve][2]
+
+resolveStack = [charToSolve]
+for i in instructions:
+    instructions[i][1] = False
+    instructions[i][2] = ""
+
+instructions['b'][1] = True
+instructions['b'][2] = answer
+
+while len(resolveStack) != 0:
+    if canResolve(resolveStack[-1], instructions):
+        ...
+        # Resolve the item.
+        if instructions[resolveStack[-1]][1] == False:
+            resolve(resolveStack[-1], instructions)
+
+        # Remove it from the resolve stack.
+        resolveStack.pop()
+    else:
+        for _ in dependents(resolveStack[-1], instructions):
+            if _ in instructions:
+                resolveStack.append(_)
+
+print(instructions[charToSolve])
