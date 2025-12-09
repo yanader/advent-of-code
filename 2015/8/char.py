d913807363ca6348e9b538d5b234b2ec92cce32a
ste <mrbarrow@hotmail.co.uk>
2025-12-09 09:24:16 +0000

    2015:8:2

--- 2015/8/char.py
+++ 2015/8/char.py
@@ -10,28 +10,40 @@ def countStringChars(s):
     trunc = s[1:-1]
     while index < len(trunc):
         if trunc[index:index + 2] == "\\x":
-            print(trunc[index:index + 2])
             count +=1
             index +=4
         elif trunc[index:index + 2] == "\\\"" or trunc[index:index + 2] == "\\\\":
-            print(trunc[index:index + 2])
             count +=1
             index +=2
         else:
-            print(trunc[index])
             count +=1
             index +=1
     return count
 
+def countEncodeChars(s):
+    count = 0
+    for _ in s:
+        if _ == "\"" or _ == "\\":
+            count += 2
+        else:
+            count += 1
+    return count + 2
+
+
+
+encodeChars = 0
 codeChars = 0
 stringChars = 0
 
 for item in data:
     codeChars += countCodeChars(item)
     stringChars += countStringChars(item)
+    encodeChars += countEncodeChars(item)
 
-print(codeChars)
+print(encodeChars)
 print(stringChars)       
 
-print(codeChars - stringChars)
+print(encodeChars - codeChars)
         
+# test = data[1]
+# print(countEncodeChars(test))
