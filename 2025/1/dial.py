bcce80d4b97fe5c846e00582dde9bc311b5ef70e
ste <mrbarrow@hotmail.co.uk>
2025-12-15 13:28:34 +0000

    2015:1:2

--- 2025/1/dial.py
+++ 2025/1/dial.py
@@ -3,20 +3,50 @@ with open("input.txt", "r") as f:
 
 position = 50
 zeroCount = 0
+passedZeroCount = 0
+
+def zeroHits(position, turn, direction):
+    if direction == "L":
+        first_hit = position if position != 0 else 100
+    else:  # "R"
+        first_hit = (100 - position) if position != 0 else 100
+
+    if turn < first_hit:
+        return 0
+
+    remaining = turn - first_hit
+    return 1 + (remaining // 100)
 
 for turn in data:
-    if turn[0] == "L":
-        position -= int(turn[1:])
+    direction = turn[0]
+    rotationalAmount = int(turn[1:])
+    if direction == "L":
+        passedZeroCount += zeroHits(position, rotationalAmount, direction)
+        position -= rotationalAmount
         if position < 99:
             position = position % 100
-    elif turn[0] == "R":
-        position += int(turn[1:])
+    elif direction == "R":
+        passedZeroCount += zeroHits(position, rotationalAmount, direction)
+        position += rotationalAmount
         if position > 99:
             position = position % 100
     if position == 0:
         zeroCount += 1
 
-print(zeroCount)
+# for turn in data:
+#     direction = turn[0]
+#     rotationalAmount = int(turn[1:])
+#     if direction == 'L':
+#         for i in range(rotationalAmount):
+#             position -= 1
+#             if position == 0: passedZeroCount += 1
+#             if position == -1: position = 99
+#     if direction == 'R':
+#         for i in range(rotationalAmount):
+#             position += 1
+#             if position == 100: position = 0
+#             if position == 0: passedZeroCount += 1
+
+# 6228 is correct
+print(passedZeroCount)
     
-
-
