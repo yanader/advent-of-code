123cc8b8aa719d7d09906d9b24710831ede2822f
ste <mrbarrow@hotmail.co.uk>
2025-12-20 08:31:36 +0000

    2015:15 complete

--- 2015/15/cookie.py
+++ 2015/15/cookie.py
@@ -3,13 +3,30 @@ from ingredients import Ingredients
 
 data = importFile("input.txt")
 
-i = Ingredients()
+ing = Ingredients()
 
 for line in data:
-    i.addIngredient(line)
+    ing.addIngredient(line)
 
 
-recipe = {"Frosting": 40, "Candy": 20,"Butterscotch": 30, "Sugar": 10}
+# recipe = {"Frosting": 40, "Candy": 20,"Butterscotch": 30, "Sugar": 10}
 # recipe = {"Butterscotch": 44, "Cinnamon": 56}
 
-print(i.totalScore(recipe))
+maxScore = 0
+
+# print(ing.totalScore(recipe))
+
+for i in range(101):
+    for j in range(101):
+        for k in range(101):
+            for l in range(101):
+                teaspoons = {"Frosting": i, "Candy": j,"Butterscotch": k, "Sugar": l}
+                if i + j + k + l != 100:
+                    continue
+                if ing.calculateCalories(teaspoons) != 500:
+                    continue
+                newScore = ing.totalScore(teaspoons)
+                if newScore > maxScore:
+                    maxScore = newScore
+
+print(maxScore)
