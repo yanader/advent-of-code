00c0879c3814df169255358eea3cc2146dc4bfc6
ste <mrbarrow@hotmail.co.uk>
2025-12-21 16:45:23 +0000

    2015:16:2 complete

--- 2015/16/aunt_checker.py
+++ 2015/16/aunt_checker.py
@@ -1,16 +1,39 @@
 from helpers import *
 
-data = importFile("input.txt")
 
-# print(MESSAGE)
+def auntOne():
+    data = importFile("input.txt")
 
-for line in data:
-    sue = sueBuilder(line)
-    checker = 0
-    for key in sue.details.keys():
-        if sue.details[key] == MESSAGE[key]:
-            checker += 1
-    if checker == 3:
-        print(sue.number)
+    for line in data:
+        sue = sueBuilder(line)
+        checker = 0
+        for key in sue.details.keys():
+            if sue.details[key] == MESSAGE[key]:
+                checker += 1
+        if checker == 3:
+            print(sue.number)
+
+
+def auntTwo():
+    data = importFile("input.txt")
+
+    for line in data:
+        sue = sueBuilder(line)
+        checker = 0
+        for key in sue.details.keys():
+            if key == "cats" or key == "trees":
+                if sue.details[key] > MESSAGE[key]:
+                    checker += 1
+            elif key == "pomeranians" or key == "goldfish":
+                if sue.details[key] < MESSAGE[key]:
+                    checker += 1
+            else:
+                if sue.details[key] == MESSAGE[key]:
+                    checker += 1
+        if checker == 3:
+            print(sue.number)
+
+auntTwo()
+
 
 
